f019155ad9f72a47c7b9dda5bf6053b7412e2e17
SuperCoder <propscode254@gmail.com>
2024-01-01 14:15:24 +0100

    change middleware

--- src/middleware/basicAuth.js
+++ src/middleware/basicAuth.js
@@ -4,97 +4,35 @@ const decodeBase64 = (base64String) =>
   Buffer.from(base64String, "base64").toString();
 
 export async function useBasicAuth(req, res, next) {
+  const invalidLogin = () => {
+    res.status(401).json({
+      success: false,
+      error: "Invalid login",
+    });
+  };
+
   // auth logic
-  const authHeader = req.headers.authorization; // eg: 'Basic dG9tLnMyMzQ1NkBnbWFpbC5jb206dG9tMTIzYWJj'
-  if (!authHeader) {
-    return res.status(401).json({
-      success: false,
-      error: "Please authenticate",
-    });
-  }
+  const authHeader = req.headers.authorization; //
+  if (!authHeader) return invalidLogin;
   const [authType, authInfoBase64] = authHeader.split(" ");
-  if (authType !== "Basic" || !authInfoBase64) {
-    return res.status(401).json({
-      success: false,
-      error: "Please authenticate using basic auth",
-    });
-  }
+  if (authType !== "Basic" || !authInfoBase64) return invalidLogin;
 
   /// base64 -> klartext
-  const authInfo = decodeBase64(authInfoBase64); // eg: 'tom.s23456@gmail.com:tom123abc'
-  const [email, password] = authInfo.split(":"); // ['tom.s23456@gmail.com', 'tom123abc']
-  if (!email || !password) {
-    return res.status(401).json({
-      success: false,
-      error: "Please authenticate using basic auth",
-    });
-  }
+  const authInfo = decodeBase64(authInfoBase64); //
+  const [email, password] = authInfo.split(":"); //
+  if (!email || !password) return invalidLogin;
 
   // Email & Password verification
   const user = await User.findOne({ email });
-  if (!user) {
-    return res.status(401).json({
-      success: false,
-      error: "Unknown user with email " + email,
-    });
-  }
+  if (!user) return invalidLogin;
 
   const passwordMatch = user.password === password;
-  if (!passwordMatch) {
-    return res.status(401).json({
-      success: false,
-      error: "Invalid password",
-    });
-  }
+  if (!passwordMatch) return invalidLogin;
+
   next(); // geh zum eigentlichen request handler
 }
 
-// console.log("headers", req.headers);
-// // auth logic
-// const authHeader = req.headers.authorization;
-// const [authType, authInfoBase64] = authHeader.split(" ");
-// // Wenn kein Autorisieruns
-// if (!authHeader) {
-//   return res.status(401).json({
-//     success: false,
-//     error: "Please authenticate",
-//   });
-// }
-// // wenn nicht die richtige Autentifizierung ist
-// if (authType !== "Basic" || !authInfoBase64) {
-//   return res.status(401).json({
-//     success: false,
-//     error: "Please authenticate using basic auth",
-//   });
-// }
-
-// // base 64 ->Klartext->
-
-// const authInfo = decodeBase64(authInfoBase64); //maria@hotmail.com:123maria
-// const [email, password] = authInfo.split(":"); //["maria@hotmail.com", "123maria"]
-
-// if (!email || !password) {
-//   return res.status(401).jsonp({
-//     success: false,
-//     error: "Please authenticate using basic auth",
-//   });
-// }
-
-// // Email & Password verification
-
-// const user = await User.findOne({ email });
-// if (!user) {
-//   return res.status(401).jsonp({
-//     success: false,
-//     error: "Unknow User with email" + email,
-//   });
-// }
-
-// const passwordMatch = user.password === password;
-
-// if (!passwordMatch) {
-//   return res.status(401).jsonp({
-//     success: false,
-//     error: "Invalid Password",
-//   });
-// }
+//1.- Wenn password falsch ausgegeben werden, sollten nicht allzu viele informationen geben warum, hacker nutzen diese als info. UN user wissen schon was si falsch gemacht haben-
+// Also Ändern alle error meldungen auf Invalid Login, also alle weg // error: "Invalid password",// error: "Unknown user with email " + email,// error: "Please authenticate using basic auth",
+// error: "Please authenticate using basic auth", // error: "Please authenticate",
+//2.- Kompakterer Code mit :INVALID FUNCTION statt immer wieder der error handling zu wiederholen
